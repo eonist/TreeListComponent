292ed243a7896546cdb83510722bb180b7b9ad3a
Eonist <unicornhive@gmail.com>
2016-03-26 15:02:22 +0100

    Files modified: 1
    Modified 1 file:src/win/MainView.swift

--- src/win/MainView.swift
+++ src/win/MainView.swift
@@ -14,6 +14,17 @@ class MainView:CustomView {
     func createTreeList(){
         //create the tree list here
         let xml:NSXMLElement = FileParser.xml("~/Desktop/assets/xml/treelist.xml")
+        let treeList:TreeList = container!.addSubView(TreeList(NaN, NaN, NaN, Node(xml),container))
+        treeList
+        //Swift.print("selectedIndex: " + TreeListParser.selectedIndex(treeList));
+        //Swift.print("selected: " + TreeListParser.selected(treeList));
+        //Swift.print("selected Title: "+DatabaseParser.attributesAt(treeList.database, TreeListParser.selectedIndex(treeList))["title"]);
+        //TreeListModifier.collapseAt(treeList, []);
+        //TreeListModifier.setSelectedAt(treeList, [1]);
+        //TreeListModifier.unSelectAll(treeList);
+        //TreeListModifier.explodeAt(treeList,[]);
+        //TreeListModifier.removeAt(treeList, [1]);
+        //treeList.database.addAt([1], new XML(<item title="Fish"/>));/*new*/
     }
     override func createTitleBar() {
         StyleManager.addStylesByURL("~/Desktop/css/titleBar.css")
