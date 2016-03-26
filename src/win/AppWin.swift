c550e6b6537ec550f3b4e1d0b36783d338c4a9fe
Eonist <unicornhive@gmail.com>
2016-03-26 14:07:39 +0100

    Files modified: 1
    Modified 1 file:src/win/AppWin.swift

--- src/win/AppWin.swift
+++ src/win/AppWin.swift
@@ -1,5 +1,10 @@
 import Foundation
 
-class AppWin {
+class AppWin:Window {
 
+    override func resolveSkin() {
+        Swift.print("frame.width: " + "\(frame.width)")
+        Swift.print("frame.height: " + "\(frame.height)")
+        self.contentView = MainView(frame.width,frame.height)/*Sets the mainview of the window*/
+    }
 }
