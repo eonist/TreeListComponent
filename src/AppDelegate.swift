7231e0a4eab7e5e57c9fa045b01fd8f8d6aeba76
Eonist <unicornhive@gmail.com>
2016-03-26 14:48:30 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -3,12 +3,13 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     weak var window: NSWindow!
+    var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
         Swift.print("hello world")
-        
+        StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")
         //continue here
-        
+        win = AppWin(300,400)
     }
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
