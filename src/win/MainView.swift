ab9e126a144c3735e686a4d0ffccd3fd9aec1983
Eonist <unicornhive@gmail.com>
2016-03-28 09:43:27 +0200

    Files modified: 1
    Modified 1 file:src/win/MainView.swift

--- src/win/MainView.swift
+++ src/win/MainView.swift
@@ -19,19 +19,19 @@ class MainView:CustomView {
         let xml:NSXMLElement = FileParser.xml("~/Desktop/assets/xml/treelist.xml")
         let treeList:TreeList = container!.addSubView(TreeList(140, 288, 24, Node(xml),container))
         treeList
-        
+        /*
+
         Swift.print("selected: " + "\(TreeListParser.selected(treeList))")
         Swift.print("selectedIndex: " + "\(TreeListParser.selectedIndex(treeList))")//Output:  [2,2,0]
         Swift.print("selected Title: " + "\(XMLParser.attributesAt(treeList.node.xml, TreeListParser.selectedIndex(treeList))!["title"])")//Output: Oregano
         TreeListModifier.unSelectAll(treeList)
-        TreeListModifier.selectAt(treeList, [1])
+        TreeListModifier.selectAt(treeList, [2])
         TreeListModifier.collapseAt(treeList, [])//closes the treeList
         TreeListModifier.explodeAt(treeList,[])//opens the treeList
         
         TreeListModifier.removeAt(treeList, [1])
         treeList.node.addAt([1],  NSXMLElement("<item title=\"Fish\"/>"))/*new*/
-        /*
-
+        
         */
         
         //continue here: Test if the output xml looks good, and keeps all the selected, closed etc. and contextual attributes.
