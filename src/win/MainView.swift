cab66d009245e2d0055a2a2929e6db818b83fb70
Eonist <unicornhive@gmail.com>
2016-03-28 09:24:20 +0200

    Files modified: 1
    Modified 1 file:src/win/MainView.swift

--- src/win/MainView.swift
+++ src/win/MainView.swift
@@ -20,12 +20,14 @@ class MainView:CustomView {
         let treeList:TreeList = container!.addSubView(TreeList(140, 288, 24, Node(xml),container))
         treeList
         
-        Swift.print("selectedIndex: " + "\(TreeListParser.selectedIndex(treeList))")
-        Swift.print("selected Title: " + "\(XMLParser.attributesAt(treeList.node.xml, TreeListParser.selectedIndex(treeList))!["title"])")
+        Swift.print("selectedIndex: " + "\(TreeListParser.selectedIndex(treeList))")//Output:  [2,2,0]
+        Swift.print("selected Title: " + "\(XMLParser.attributesAt(treeList.node.xml, TreeListParser.selectedIndex(treeList))!["title"])")//Output: Oregano
+        
+        TreeListModifier.collapseAt(treeList, [])
+        
         /*
         Swift.print("selected: " + "\(TreeListParser.selected(treeList))")
         
-        TreeListModifier.collapseAt(treeList, [])
         TreeListModifier.setSelectedAt(treeList, [1])
         TreeListModifier.unSelectAll(treeList)
         TreeListModifier.explodeAt(treeList,[])
