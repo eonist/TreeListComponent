f5591674c34edd988165cc312b49f767fe4b0182
Eonist <unicornhive@gmail.com>
2016-04-15 08:32:33 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -5,14 +5,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     weak var window: NSWindow!
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
         //Swift.print("hello world")
         StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")
         //continue here
         win = AppWin(300,400)
         let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
         app.windows[0].close()/*close the initial non-optional default window*/
-        
     }
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
